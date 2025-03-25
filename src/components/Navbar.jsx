199f95225e42c265ac769b31bbaaac05b3482fa2
Tanuj Singh <tanujsingh407@gmail.com>
2025-03-25 15:05:25 +0530

    logo pic updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <nav className="navbar bg-white">
                 <div className="container-fluid px-4">
                     <a className="navbar-brand d-flex align-items-center" href="#">
-                        <img src="src\assets\logo_rapI854J.png" alt="Logo" className="d-inline-block align-text-top me-3" />
+                        <img src="src\assets\logo_rapI854J.jpg" alt="Logo" className="d-inline-block align-text-top me-3" />
                         Admin
                     </a>
                 </div>
